d5f9b2c14a9c7fb35cd170fdd3941874d99ed5ea
schmittertj05 <schmittertj05@gmail.com>
2026-03-08 18:29:59 +0100

    Update index.tsx

--- app/config/index.tsx
+++ app/config/index.tsx
@@ -1,12 +1,7 @@
-import { cookieStorage, createStorage } from "wagmi";
-import { WagmiAdapter } from "@reown/appkit-adapter-wagmi";
-import { mainnet, arbitrum } from "@reown/appkit/networks";
+import { WagmiAdapter } from "@reown/appkit-adapter-wagmi"
+import { mainnet, arbitrum } from "@reown/appkit/networks"
 
-export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID
-
-if (!projectId) {
-    throw new Error("Project id is not defined.")
-}
+export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID || "demo"
 
 export const networks = [mainnet, arbitrum]
 
@@ -15,6 +10,5 @@ export const wagmiAdapter = new WagmiAdapter({
   networks,
   projectId,
 })
-
 
 export const config = wagmiAdapter.wagmiConfig
